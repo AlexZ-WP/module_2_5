c2266b17caa81dbe3251648ffb4b8434bf8c2849
AlexZ-WP <alexz10@mail.ru>
2024-10-02 23:16:04 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,17 @@
+def get_matrix(n, m, value):
+    matrix = []
+    i = n
+    j = m
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+            matrix.append(row)
+
+        return matrix
+matrix = get_matrix(2, 2, value=10)
+print(matrix)
+matrix = get_matrix(3, 5, value=42)
+print(matrix)
+matrix = get_matrix(4, 2, value=13)
+print(matrix)
